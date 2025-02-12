2b7d50ef9206748a76d8721b0874eb26a17dee6b
taherihamed <taheri.hamed70@gmail.com>
2025-02-12 17:04:49 +0330

    update main

--- main.py
+++ main.py
@@ -12,16 +12,55 @@ def main():
     ]
 
 def solve (board):
-    pass
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
 
 
 def valid (board, position, number):
-    pass
+    # Check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # Check box
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
+    return True
 
 
 def find_empty (board):
-    pass
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
 
 
-def print_board(board):
+def print_board (board):
     pass
